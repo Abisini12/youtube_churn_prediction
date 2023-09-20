a1df2a8231885f606b8013c995c2b1e486ced4c7
Abisini12 <117000300+Abisini12@users.noreply.github.com>
2023-09-20 22:23:50 +0530

    Update webapplication.py

--- webapplication.py
+++ webapplication.py
@@ -23,18 +23,59 @@ if response.status_code == 200:
 else:
     print("Failed to fetch the pickle file from GitHub")
 
-# Create a function for prediction
+# Function to preprocess input data and make predictions
 def churn_prediction(input_data):
-    
-    input_data_as_numpy_array = np.asarray(input_data).astype(float)  # Convert input data to float
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    prediction = loaded_model.predict(input_data_reshaped)
-    if prediction[0] == 0:
-        return 'The subscriber is predicted as not leaving the channel..'
-    else:
-        return 'The subscriber is predicted to unsubscribe the channel..'
+    try:
+        # Map 'Gender' and 'Genre' to numerical values using one-hot encoding
+        gender_mapping = {'Male': 0, 'Female': 1}
+        genre_mapping = {'Sports': 0, 'Series': 1, 'Education': 2, 'Entertainment': 3}  # Update with your genre categories
+        
+        # Preprocess input data
+        gender = gender_mapping.get(input_data[0], 0)  # Default to 0 if not found in mapping
+        genre = genre_mapping.get(input_data[1], 0)  # Default to 0 if not found in mapping
+        num_of_videos = float(input_data[2])
+        like_dislike = float(input_data[3])
+        is_active_member = float(input_data[4])
+        age = float(input_data[5])
+        streamed_time = float(input_data[6])
+
+        # Create a NumPy array with the preprocessed data
+        input_data_as_numpy_array = np.array([gender, genre, num_of_videos, like_dislike, is_active_member, age, streamed_time])
+
+        # Reshape the data and make the prediction
+        input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+        prediction = loaded_model.predict(input_data_reshaped)
+
+        if prediction[0] == 0:
+            return 'The subscriber is predicted as not leaving the channel..'
+        else:
+            return 'The subscriber is predicted to unsubscribe the channel..'
+    except ValueError:
+        return 'Invalid input. Please enter valid values for all fields.'
 
 def main():
+    # Setting Application title
+    st.title('Youtube Subscriber Churn Prediction')
+
+    Gender = st.selectbox('Select the gender:', ('Male', 'Female'))
+    Genre = st.selectbox('Select the genre:', ('Sports', 'Series', 'Education', 'Entertainment'))
+    NumOfVideo = st.number_input('The number of videos watched:')
+    Like_Dislike = st.number_input('Did the subscriber like the video?')
+    IsActiveMember = st.number_input('Is the subscriber active?')
+    Age = st.number_input('Enter the age:')
+    Streamedtime = st.number_input('Enter the streamed time of the subscriber:')
+
+    # Code for prediction
+    diagnosis = ''
+    
+    # Creating a button for prediction
+    if st.button('Predict'):
+        diagnosis = churn_prediction([Gender, Genre, NumOfVideo, Like_Dislike, IsActiveMember, Age, Streamedtime])
+    st.success(diagnosis)
+
+if __name__ == '__main__':
+    main()
+
     # Setting Application title
     st.title('Youtube Subscriber Churn Prediction')
 
